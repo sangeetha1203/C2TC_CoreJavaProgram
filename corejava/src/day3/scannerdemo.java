f3051f66a63911535cb60f7a7d44fcd404c2b304
rmahe <rsangeethar126@gmail.com>
2025-10-03 17:40:16 +0530

    this is sample java code concepts

---
+++ corejava/src/day3/scannerdemo.java
@@ -0,0 +1,16 @@
+package day3;
+import java.util.Scanner;
+
+public class scannerdemo {
+
+	public static void main(String[] args) {
+		System.out.print(" ");
+		Scanner sc= new Scanner(System.in);
+		String name=sc.nextLine();
+		System.out.println(name);
+		
+		
+
+	}
+
+}
