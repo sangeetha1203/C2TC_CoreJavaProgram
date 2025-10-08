d358ad6a787934fc9c026e819e663d643913e02d
rmahe <rsangeethar126@gmail.com>
2025-10-08 21:45:14 +0530

    sample java concepts programs...

---
+++ corejava/src/com/ifet/dayfive/multilevelinheritance/texteditor/Notepad.java
@@ -0,0 +1,28 @@
+package com.ifet.dayfive.multilevelinheritance.texteditor;
+
+
+	public class Notepad {
+	    protected String content;
+
+	    public Notepad() {
+	        this.content = "";
+	    }
+
+	    public void write(String text) {
+	        content += text; //content = content+text
+	    }
+
+	    public void displayContent() {
+	        System.out.println("Notepad Content: " + content);
+	    }
+	    
+	    
+	    /*
+	     * returntype funcName()
+	     * {
+	     * 		//statement
+	     * }
+	     * 
+	     * */
+	}
+
